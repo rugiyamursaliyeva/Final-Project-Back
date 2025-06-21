d30e0e4e9363ed85514f00055cd0f2173417cfd0
rugiyamursaliyeva <ruqiyemurseliyeva@gmail.com>
2025-06-21 23:18:59 +0400

    the admin-login added

--- server.js
+++ server.js
@@ -8,9 +8,28 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors('*'))
 
+const adminUser = {
+  username: 'admin',
+  password: '1234',
+}
+
+
+app.post('/admin-login', (req, res) => {
+  const { username, password } = req.body
+
+  if (username === adminUser.username && password === adminUser.password) {
+    return res.status(200).json({ success: true, message: 'Admin girişi uğurludur' })
+  } else {
+    return res.status(401).json({ success: false, message: 'Yanlış istifadəçi adı və ya şifrə' })
+  }
+})
+
+
 app.use('/', ProductRoutes)
 
+
 connectDB()
+
 app.listen(5000, () => {
-    console.log('backend running');
+  console.log('backend running');
 })
